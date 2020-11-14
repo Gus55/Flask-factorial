61c8f5859395485f5182f89fbb51ccda0207abf1
Gus55 <38430566+Gus55@users.noreply.github.com>
2020-11-13 19:16:41 -0600

    Programa en python y archivos necesarios para cloud

---
+++ main.py
@@ -0,0 +1,41 @@
+#main.py
+from flask import Flask, jsonify, request
+from math import factorial
+
+app = Flask(__name__)
+
+lfact = []
+
+@app.route('/<number>')
+def home(number):
+  number=int(number)
+  factorial_total = 1
+  while number > 1:
+      factorial_total *= number
+      number -= 1
+
+  lfact = []
+  facto = {
+      "Factorial": factorial_total ,
+  }
+  lfact.append(facto)
+  return jsonify(lfact)
+
+@app.route('/<number>', methods=['POST'])
+def add_fact(number):
+    number=int(number)
+    factorial_total = 1
+    while number > 1:
+        factorial_total *= number
+        number -= 1
+
+    lfact = []
+    facto = {
+        "Factorial": factorial_total ,
+    }
+    lfact.append(facto)
+    return jsonify(lfact)
+    #return jsonify(number)
+
+if __name__ == '__main__':
+  app.run(debug=True)
